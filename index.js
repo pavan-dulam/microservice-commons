ef0a2bfd17451a231487bf7a3675ee5cf646edfc
pavan-dulam <pavandulam16@gmail.com>
2024-04-12 13:28:46 +0530

    fix: git workflow action

--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  logger: require('./src/logger'),
-  dateAndTime: require('./src/dateTime'),
-  objectUtils: require('./src/objectUtils'),
-  apiCaller: require('./api-caller'),
-  encryption: require('./src/encryption'),
-  filenames: require('./src/filenames'),
-};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  logger: require('./logger'),
+  dateAndTime: require('./dateTime'),
+  objectUtils: require('./objectUtils'),
+  apiCaller: require('./apiCaller'),
+  encryption: require('./encryption'),
+  filenames: require('./filenames'),
+};
